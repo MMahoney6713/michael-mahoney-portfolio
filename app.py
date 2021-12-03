6fe0ec2d0939a8d3646fc667eb6e25ada9c01e7a
mmahoney6713 <mrm257@cornell.edu>
2021-12-02 17:26:16 -0800

    moved secret to config.py and preparing for move to rpi

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
+app.config.from_pyfile('config.py')
 
 
 @app.route("/")
@@ -13,4 +13,4 @@ def homepage():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80)
