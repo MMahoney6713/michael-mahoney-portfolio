deadd327da05ac2ae0e8b20d5c059cf5ce19031c
mmahoney6713 <mrm257@cornell.edu>
2021-12-06 14:57:30 -0800

    adding flask templating to make individual pages more succinct

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ def homepage():
 
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
 
+    return render_template("projects.html")
+
+@app.route("/about")
+def about():
+
+    return render_template("about-me.html")
+
+@app.route("/resume")
+def resume():
+
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
